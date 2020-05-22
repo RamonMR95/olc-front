79ed550f0d2b368bc36ba14ace888d59f659875b
Ramon Moñino <ramonmr16@gmail.com>
2020-05-22 14:58:20 +0200

    Added users route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { UsersComponent } from "./components/users/users.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/users", pathMatch: "full" },
+  { path: "users", component: UsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
