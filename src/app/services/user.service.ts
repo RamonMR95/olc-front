9eee857688bac4ea39f2ea7db7112ef1044ccea0
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-05-26 20:31:13 +0200

    Added interface for get marks student, fixed API url  and added method for get marks.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_URL } from "../config/config";
 import { User } from "../models/user.model";
+import { UserMarksSubjects } from '../interfaces/user.marks.interface';
 
 @Injectable({
   providedIn: "root",
@@ -10,24 +11,29 @@ export class UserService {
   constructor(private httpClient: HttpClient) {}
 
   getUsers(): Promise<any> {
-    return this.httpClient.get<any>(`${API_URL}/users`).toPromise();
+    return this.httpClient.get<any>(`${API_URL}/users/list`).toPromise();
   }
 
   getUser(id: number): Promise<User> {
-    return this.httpClient.get<User>(`${API_URL}/user?id=${id}`).toPromise();
+    return this.httpClient.get<User>(`${API_URL}/users?id=${id}`).toPromise();
   }
 
   createUser(user: User): Promise<User> {
-    return this.httpClient.post<User>(`${API_URL}/create`, user).toPromise();
+    return this.httpClient.post<User>(`${API_URL}/users/create`, user).toPromise();
   }
 
   updateUser(user: User, id: number): Promise<User> {
     return this.httpClient
-      .put<User>(`${API_URL}/user?id=${id}`, user)
+      .put<User>(`${API_URL}/users?id=${id}`, user)
       .toPromise();
   }
 
   deleteUser(id: number): Promise<User> {
-    return this.httpClient.delete<User>(`${API_URL}/user?id=${id}`).toPromise();
+    return this.httpClient.delete<User>(`${API_URL}/users?id=${id}`).toPromise();
   }
+
+  getUserMarksByIdAndDate(id: number, date: string): Promise<UserMarksSubjects> {
+    return this.httpClient.get<UserMarksSubjects>(`${API_URL}/users/marks?id=${id}&year_start=${date}`).toPromise();
+  }
+
 }
