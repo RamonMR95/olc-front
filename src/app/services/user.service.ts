832da3464e02a34621e7d1825811d4fa8419b93e
Ramon Moñino <ramonmr16@gmail.com>
2020-06-01 22:05:31 +0200

    Added new services.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,24 +10,24 @@ export class UserService {
   constructor(private httpClient: HttpClient) {}
 
   getUsers(): Promise<any> {
-    return this.httpClient.get<any>(`${API_URL}/users`).toPromise();
+    return this.httpClient.get<any>(`${API_URL}/users/list`).toPromise();
   }
 
   getUser(id: number): Promise<User> {
-    return this.httpClient.get<User>(`${API_URL}/user?id=${id}`).toPromise();
+    return this.httpClient.get<User>(`${API_URL}/users?id=${id}`).toPromise();
   }
 
   createUser(user: User): Promise<User> {
-    return this.httpClient.post<User>(`${API_URL}/create`, user).toPromise();
+    return this.httpClient.post<User>(`${API_URL}/users/create`, user).toPromise();
   }
 
   updateUser(user: User, id: number): Promise<User> {
     return this.httpClient
-      .put<User>(`${API_URL}/user?id=${id}`, user)
+      .put<User>(`${API_URL}/users?id=${id}`, user)
       .toPromise();
   }
 
   deleteUser(id: number): Promise<User> {
-    return this.httpClient.delete<User>(`${API_URL}/user?id=${id}`).toPromise();
+    return this.httpClient.delete<User>(`${API_URL}/users?id=${id}`).toPromise();
   }
 }
