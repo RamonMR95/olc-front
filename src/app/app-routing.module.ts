5b1c46a50ad5e76d85fb5238c472c7117d78ee19
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-06-02 22:56:35 +0200

    Added Components and route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { UsersComponent } from "./components/users/users.component";
 import { LoginComponent } from "./components/login/login.component";
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from "./components/register/register.component";
 import { EnrollmentListComponent } from "./components/enrollment-list/enrollment-list.component";
 import { AuthGuard } from "./guards/auth.guard";
@@ -10,9 +11,10 @@ import { AuthGuard } from "./guards/auth.guard";
 const routes: Routes = [
   { path: "", redirectTo: "/users", pathMatch: "full" },
   { path: "users", component: UsersComponent, canActivate: [AuthGuard] },
+  { path: "profile/:id", component: ProfileComponent, canActivate: [AuthGuard]},
+  { path: "enrollment", component: EnrollmentListComponent, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "enrollment", component: EnrollmentListComponent, canActivate: [AuthGuard]  },
+  { path: "register", component: RegisterComponent }
 ];
 
 @NgModule({
