e9cca292e0847070760adfcad9f33b87fef930a0
Ramon Moñino <ramonmr16@gmail.com>
2020-06-05 14:26:32 +0200

    Added news model.

---
+++ src/app/models/news.model.ts
@@ -0,0 +1,10 @@
+export class News {
+  public id: number;
+
+  constructor(
+    public title: string,
+    public content: string,
+    public writer: string,
+    public createdAt: Date
+  ) {}
+}
