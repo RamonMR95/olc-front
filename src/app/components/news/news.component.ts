160bbb2b445e5647c2dc9bf24114c7b33289fa53
Ramon Moñino <ramonmr16@gmail.com>
2020-06-05 14:26:57 +0200

    Added news component.

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "../../services/news.service";
+import { News } from "src/app/models/news.model";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.scss"],
 })
 export class NewsComponent implements OnInit {
+  news: News[];
 
-  constructor() { }
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
+    this.loadNews();
   }
 
+  loadNews(): void {
+    this.newsService.getLast5News().then((nws) => (this.news = nws));
+  }
 }
