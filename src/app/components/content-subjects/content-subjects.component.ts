7439d4092a19e78f9506db83d3391181659e9a35
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-06-05 11:00:20 +0200

    Added components to content-subject

---
+++ src/app/components/content-subjects/content-subjects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-subjects',
+  templateUrl: './content-subjects.component.html',
+  styleUrls: ['./content-subjects.component.scss']
+})
+export class ContentSubjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
