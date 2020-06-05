9a558d8a3581707e7225a37771008fc5015a2e74
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-06-05 15:48:03 +0200

    Add subject service

--- src/app/services/subject.service.ts
+++ src/app/services/subject.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_URL } from '../config/config';
-import { Subject } from '../models/subject.model';
+import { SubjectTheme } from '../interfaces/subject.theme.interface';
 
 @Injectable({
   providedIn: "root",
@@ -12,6 +12,18 @@ export class SubjectService {
   
   getSubjectsByCourseId(courseId: number): Promise<any> {
       return this.httpClient.get<any>(`${API_URL}/courses/subjects?course_id=${courseId}`).toPromise();
+  }
+
+  getThemeBySubjectsName(name: string): Promise<any> {
+    return this.httpClient.get<any>(`${API_URL}/subject/topic?name=${name}`).toPromise();
+  }
+
+  getContentThemeByThemeId(topicId: number): Promise<any> {
+    return this.httpClient.get<any>(`${API_URL}/subject/topicContent?topic_id=${topicId}`).toPromise();
+  }
+
+  getAllTheme(): Promise<any> {
+    return this.httpClient.get<any>(`${API_URL}/subject/topic/list`).toPromise();
   }
   
 }
