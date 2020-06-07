fda941545cd0381cc0b1e5cfa7aa78ad41d0334c
Ramon Moñino <ramonmr16@gmail.com>
2020-06-07 10:14:20 +0200

    Finished exam model.

--- src/app/models/exam.model.ts
+++ src/app/models/exam.model.ts
@@ -1,3 +1,13 @@
+import { Subject } from "src/app/models/subject.model";
+import { Course } from "./course.model";
 export class Exam {
-    public id: number;
+  public id: number;
+
+  constructor(
+    public name?: string,
+    public subject?: Subject,
+    public course?: Course,
+    public date?: Date,
+    public visible?: boolean
+  ) {}
 }
