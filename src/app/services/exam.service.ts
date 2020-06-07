114a440c15d7c1db038bb81301379893ac8ca89f
Ramon Moñino <ramonmr16@gmail.com>
2020-06-07 10:14:04 +0200

    Method to the get the not done exams by user id and course id.

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_URL } from "../config/config";
-import { UserExam } from '../models/user.exam.model';
+import { UserExam } from "../models/user.exam.model";
+import { Exam } from "../models/exam.model";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -17,20 +20,24 @@ export class ExamService {
 
   getExamByExamId(examId: number): Promise<any> {
     return this.httpClient
-    .get<any>(`${API_URL}/exams?id=${examId}`)
-    .toPromise();
+      .get<any>(`${API_URL}/exams?id=${examId}`)
+      .toPromise();
   }
 
-  submitExam(userExam: UserExam): Promise<any>{
+  submitExam(userExam: UserExam): Promise<any> {
     return this.httpClient
-    .post<any>(`${API_URL}/user-exam`, userExam)
-    .toPromise();
+      .post<any>(`${API_URL}/user-exam`, userExam)
+      .toPromise();
   }
 
   getUserExamByUserId(userId: number, examId: number): Promise<any> {
     return this.httpClient
-    .get<any>(`${API_URL}/user-exam/exam?user_id=${userId}&exam_id=${examId}`)
-    .toPromise();
+      .get<any>(`${API_URL}/user-exam/exam?user_id=${userId}&exam_id=${examId}`)
+      .toPromise();
   }
 
+  getExamsByUserIdAndCourseId(userId: number, courseId: number): Observable<Exam[]> {
+    return this.httpClient
+      .get<Exam[]>(`${API_URL}/exams/not-done?user_id=${userId}&course_id=${courseId}`);
+  }
 }
